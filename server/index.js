fa12e4aa42fcdb55dfe82810b1f7d6366af9cf2e
Sphinx-Von <rubinahakim95@gmail.com>
2025-07-19 20:54:57 +0530

    second c

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // Vite frontend
+  origin: process.env.FRONTEND_URL, // Vite frontend
   credentials: true
 }));
 app.use(express.json());
